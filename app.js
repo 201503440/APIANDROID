abb4ddae58bcffebf2a382280b4c29d83ccc9d60
201503440 <csazo171@gmail.com>
2018-11-08 23:30:22 -0600

    'end1010'

--- app.js
+++ app.js
@@ -1,18 +1,80 @@
-var http = require('http');
+// Cargar modulos y crear nueva aplicacion
+var express = require("express"); 
+var app = express();
 var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // soporte para bodies codificados en jsonsupport
+app.use(bodyParser.urlencoded({ extended: true })); // soporte para bodies codificados
+ 
+var cilinder = 'c';
+var piramide = 'p';
+var heart = 'h';
+var cube = 'u';
+var text = ''; 
 
-http.createServer(function (req, res) {
-    
-    if (req.method == 'POST') {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end('SE HIZO PETICION POST AL SERVIDOR...');
-    }else if (req.method == 'GET') {        
-      res.writeHead(200, { 'Content-Type': 'text/html' });        
-      res.send('a');
-      res.end('PETICION GET SERVIDOR CORRIENDO...');      
-    }else {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end('Modulo de seguridad interna corriendo...');
-    }
-    
-}).listen(process.env.PORT || 8080);
+app.get('/Android/Piramide', function(req, res, next) {
+  piramide = 'P';
+  res.send('Piramide Android');
+});
+
+app.get('/Arduino/Piramide', function(req, res, next){
+    res.send(piramide);
+    piramide = 'p';
+});
+ 
+app.get('/Android/Cilinder', function(req, res, next) {
+  cilinder = 'C';
+  res.send('Cilinder Android');
+});
+
+app.get('/Arduino/Cilinder', function(req, res, next){
+    res.send(cilinder);
+    cilinder = 'c';
+});
+
+app.get('/Android/Heart', function(req, res, next) {
+  heart = 'H';
+  res.send('Heard Android');
+});
+
+app.get('/Arduino/Heart', function(req, res, next){
+    res.send(heart);
+    heart = 'h';
+});
+
+app.get('/Android/Cube', function(req, res, next) {
+  cube = 'U';
+  res.send('Cube Android');
+});
+
+app.get('/Arduino/Cube', function(req, res, next){
+  res.send(cube);  
+  cube = 'u';
+  //linea extre CS  
+  res.end('c:');
+});
+/*//Ejemplo: POST http://localhost:8080/items
+app.post('/items', function(req, res) {
+   var data = req.body.data;
+   res.send('Add ' + data);
+});
+ 
+//Ejemplo: PUT http://localhost:8080/items
+app.put('/items', function(req, res) {
+   var itemId = req.body.id;
+   var data = req.body.data;
+   res.send('Update ' + itemId + ' with ' + data);
+});
+ 
+//Ejemplo: DELETE http://localhost:8080/items
+app.delete('/items/:id', function(req, res) {
+   var itemId = req.params.id;
+   res.send('Delete ' + itemId);
+});*/
+  
+var server = app.listen(8080, function (req, res) {
+
+  //console.log('Server is running..'); 
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('SERVIDOR CORRIENDO...');
+
+});
