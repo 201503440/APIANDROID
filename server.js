123463274f799789a7ab83fc887423f268c5299a
201503440 <csazo171@gmail.com>
2018-11-08 18:41:38 -0600

    'other12'

---
+++ server.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var bodyParser = require('body-parser');
+
+http.createServer(function (req, res) {
+    
+    if (req.method == 'POST') {
+    	res.writeHead(200, { 'Content-Type': 'text/html' });
+    	res.end('SE HIZO PETICION POST AL SERVIDOR...');
+    }else if (req.method == 'GET') {
+    	res.writeHead(200, { 'Content-Type': 'text/html' });
+    	res.end('SE HIZO PETICION GET AL SERVIDOR...');
+    }else {
+    	res.writeHead(200, { 'Content-Type': 'text/html' });
+    	res.end('Modulo de seguridad interna corriendo...');
+    }
+    
+}).listen(process.env.PORT || 8080);
